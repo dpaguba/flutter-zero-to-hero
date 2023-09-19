d302129b74a92af31a89c3101792f7fc380d2bf5
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-20 00:48:41 +0200

    added const file

--- tic_tac_toe_app/lib/pages/home.dart
+++ tic_tac_toe_app/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tic_tac_toe_app/utils/constants.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,8 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: backgroundColor,
+    );
   }
 }
