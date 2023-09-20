57f0ef13f2ce88ab95aae1968e30ea5628a7ef21
Dmytro Pahuba <dpaguba@gmail.com>
2023-09-20 04:00:52 +0200

    comment unused import

--- tic_tac_toe_app/lib/main.dart
+++ tic_tac_toe_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+// import 'package:flutter/services.dart';
 import 'package:tic_tac_toe_app/pages/intro.dart';
 
 void main() {
